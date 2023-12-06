8c811ee9e260b0f42638435440a7a84f83e3dfd1
WF11242 <webdev123487@outlook.com>
2023-12-06 09:35:34 -0500

    Added HTML

--- HeadFirstLounge/battleship.js
+++ HeadFirstLounge/battleship.js
@@ -15,5 +15,12 @@ while (isSunk == false) {
     } else {
         guesses = guesses + 1;
     }
+    if (guess == location1){
+        hits = hits + 1;
+    } else if (guess == location2){
+        hits = hits + 1;
+    } else if (guess == location3){
+        hits = hits + 1;
+    }
 }
 
