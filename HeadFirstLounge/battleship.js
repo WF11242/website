50b6f55d6569c7aa94b813a5e5df10fc9f763bef
WF11242 <webdev123487@outlook.com>
2023-12-06 09:43:27 -0500

    Added JS

--- HeadFirstLounge/battleship.js
+++ HeadFirstLounge/battleship.js
@@ -17,6 +17,12 @@ while (isSunk == false) {
     
         if (guess == location1 || guess == location2 || guess == location3) {
             hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk == true;
+
+                alert("You've sunk my battleship!");
+            }
         }
     }
 }
