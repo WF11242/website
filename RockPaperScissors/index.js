a9e07194750b54cd5d2d649c213f9de7bbd2af1f
Freericks <webdev123487@outlook.com>
2024-06-06 13:51:50 -0400

    added

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -15,4 +15,14 @@ if (!playerChoice) {
 playerChoice = playerChoice.toLowerCase(); 
 }; 
 
+var index = Math.floor(Math.random() * gameOptions.length); 
+var computerChoice = gameOptions[index]; 
+
+window.alert("The computer picked " + computerChoice)
+
+if (playerChoice === computerChoice) {
+    ties++; 
+    window.alert("It's a tie! Try again."); 
+}
+
 playGame (); 
