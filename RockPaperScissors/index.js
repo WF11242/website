984d220d2822cf4a178ca6a6a09308c275ddef85
Freericks <webdev123487@outlook.com>
2024-06-06 09:58:58 -0400

    Added Function

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -2,3 +2,15 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelector(".choiceBtn");
+let player;
+let computer;
+let result; 
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+}));
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 3) + 1;
+};
