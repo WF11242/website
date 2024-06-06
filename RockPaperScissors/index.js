1b0039344a10fbfb2e7b0b34b1558404860e52d9
Freericks <webdev123487@outlook.com>
2024-06-06 13:49:07 -0400

    Added JS

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,45 +1,6 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelector(".choiceBtn");
-let player;
-let computer;
-let result; 
+var wins = 0; 
+var ties = 0; 
+var losses = 0; 
 
 
-function choice(){
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = `Player : ${player}`;
-    computerText.textContent = `Computer : ${computer}`;
-    resultText.textContent = checkWinner();
-    console.log("Test")
-};
-function computerTurn(){
-    const randNum = Math.floor(Math.random() * 3) + 1;
-    switch(randNum){
-        case 1:
-            computer = "ROCK";
-            break;
-        case 2:
-            computer = "PAPER";
-            break;
-        case 3:
-            computer = "SCISSORS";
-            break;
-    };
-};
-function checkWinner(){
-    if(player == computer){
-        return "Draw!";
-    }
-    else if(computer == "ROCK"){
-        return (player == "PAPER") ? "You Win!" : "You Lose!"
-    }
-    else if(computer == "PAPER"){
-        return (player == "SCISSORS") ? "You Win!" : "You Lose!"
-    }
-    else if(computer == "SCISSORS"){
-        return (player == "ROCK") ? "You Win!" : "You Lose!"
-    }
-};
+var gameOptions = ["R", "P", "S"]; 
