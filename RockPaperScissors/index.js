578470bd7ccdb5e128e6c09722a6c6ac2d00296e
Freericks <webdev123487@outlook.com>
2024-06-06 09:50:56 -0400

    Added

---
+++ RockPaperScissors/index.js
@@ -0,0 +1,4 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelector(".choiceBtn");
