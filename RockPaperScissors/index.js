60584fd4bf78dc58a4316ed48659fcf5a3772e63
Freericks <webdev123487@outlook.com>
2024-06-06 10:08:24 -0400

    Added Function

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -13,4 +13,15 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
 
 function computerTurn(){
     const randNum = Math.floor(Math.random() * 3) + 1;
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    };
 };
